d9078afa57d916c669ddde72898fd33a84cf689c
İhsan Alptekin <36696147+alptekinihsan@users.noreply.github.com>
2022-12-07 13:56:17 +0300

    Add files via upload

---
+++ odev1.dart
@@ -0,0 +1,52 @@
+       /*   İHSAN ALPTEKİN */
+
+void main(){
+
+  String sehir = "İSTANBUL";
+  String ulke = "KANADA";
+  String telefon = "5556664499";
+  String posta_kodu = "70000";
+  String email = "asd@gmail.com";
+  String meslek = "Software Engineering";
+  int stok_miktari = 15;
+  String musteri_adi = "ihsan";
+  int bakiye = 10000;
+  String dogum_gunu = "21/05/1852";
+  int maas = 15000;
+  String medeni_durum = "Bekar";
+  String urun_yorum = "Lorem Ipsum is simply";
+  String odeme_tarihi = "01/01/2022";
+  double odeme = 1520.85;
+  int siparis_adeti = 152;
+  String araba_modeli = "Tesla Model S";
+  String kitap_adi = "Sherlock Holmes";
+  String yayinlanma_tarihi = "02/02/1998";
+  double indirim_miktari = 198.85;
+  int oda_sayisi = 55;
+  int enlem = 54;
+  int boylam = 46;
+  String urun_adi = "Klavye";
+  double yemek_fiyati = 55.98;
+  String marka = "Adidas";
+  String muzik_adi = "Tom Odell Another Love";
+  double video_suresi = 55.46;
+  double urun_puani = 4.9;
+  String resim_adi = "1.JPG";
+  String dosya_formati = "mmm.docx";
+  String renk = "Kırmızı";
+  String renk_kodu = "#000015";
+  String telefon_modeli = "Samsung A9";
+  String ekran_boyutu = "912x52";
+  double agirlik = 55.88;
+  String ulusal_gunu = "30 Ağustos Zafer Bayramı";
+  String tatil_gunu = "Pazar";
+  String rezervasyon_tarihi = "01/01/2023";
+  String sokak_adi = "Zümrüt Kale";
+  String otobus_hatti = "Gündoğdu";
+  double kalan_dakika = 55.65;
+  String takip_kodu = "Hsv1568466784";
+  int kupon_suresi = 55 ;
+  String kupon_kodu = "hsk2016354s";
+  String fatura_adresi = "Karşıyaka manki bulvarı no:564";
+
+}
